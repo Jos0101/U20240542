3c59daf88c3fee7e79b94f4e9662cdab068c2da5
Stan01 <143421342+Jos0101@users.noreply.github.com>
2024-09-08 18:48:23 -0600

    Update getDate.js

--- Formato_Fecha_Hora/getDate.js
+++ Formato_Fecha_Hora/getDate.js
@@ -1,7 +1,5 @@
-let fechaActual = new Date();
+let fechaActual = new Date().toString('sh')
 
-  let semanaActual = fechaActual.getDay();
-    let opcionesDias = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-        let diaSemana = opcionesDias[semanaActual];
+ 
 
-    console.log(diaSemana);
+ console.log(fechaActual)
